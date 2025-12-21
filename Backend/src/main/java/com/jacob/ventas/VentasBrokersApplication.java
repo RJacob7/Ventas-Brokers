a7588ed87033e5cfa2e128aaf5a4397acd4093cc
jacob <ray.osoriotorres@outlook.es>
2025-12-21 13:47:46 -0600

    se agrega el backend

---
+++ Backend/src/main/java/com/jacob/ventas/VentasBrokersApplication.java
@@ -0,0 +1,13 @@
+package com.jacob.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasBrokersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasBrokersApplication.class, args);
+	}
+
+}
